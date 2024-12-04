764a12405744647115373eed6c85565ceb1d140d
macbook <svy1dan@gmail.com>
2024-12-04 20:00:35 +0300

    Изменил метод решения.

--- taski/task1.js
+++ taski/task1.js
@@ -1,13 +1,10 @@
 // Задача: Напишите функцию addNumbers, которая принимает массив чисел и возвращает их сумму.
 
 function addNumbers(numbers) {
-    summa = 0
-    for (i = 0; i < numbers.length; i++) {
-        summa += numbers[i];
-    }
+    const summa = numbers.reduce((acc, num) => acc + num, 0);
     console.log(summa);
 }
 
-addNumbers([0, 0, 0, 1, 2])
+addNumbers([0, 0, 0, 1, 2]);
 
 // мяу
