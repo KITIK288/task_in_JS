b543927bab12b97866207bd87f5e389cc0ec43eb
macbook <svy1dan@gmail.com>
2024-12-04 19:53:04 +0300

    Решение 10 задач по программированию на языке джава срипт. Баг фикс.

--- taski/task1.js
+++ taski/task1.js
@@ -1,13 +1,13 @@
 // Задача: Напишите функцию addNumbers, которая принимает массив чисел и возвращает их сумму.
 
 function addNumbers(numbers) {
-
-    summa  = 0
-
+    summa = 0
     for (i = 0; i < numbers.length; i++) {
-        summa += i;
+        summa += numbers[i];
     }
     console.log(summa);
 }
 
-addNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+addNumbers([0, 0, 0, 1, 2])
+
+// мяу
